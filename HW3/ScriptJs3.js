88c0e63ddad31fcb946c24615a4f9b54d403e378
NikolaBlackJack <nikola716@gmail.com>
2018-10-26 14:35:48 +0300

    Revert "Merge branch 'master' of https://github.com/NikolayBlackJack/JsHW"
    This reverts commit 2601e0be8e2a9577288342ba903c06425b7af122, reversing
    changes made to 7297659f8e10bcecbc08fe63ff4db2f8f725b36b.

--- HW3/ScriptJs3.js
+++ HW3/ScriptJs3.js
@@ -1,12 +1,12 @@
-    let y = 5 ;
-    let rach = 0;
-    for (let j = 1; j <= y ; j++) {
-        let k = +prompt('Введіть число', '');
-        if (k < 0) {
-            rach++;
-        }
+let y = 5 ;
+let rach = 0;
+for (let j = 1; j <= y ; j++){
+    let k = +prompt('Введіть число','');
+    if (k < 0){
+        rach++;
     }
-    alert( rach );
+}
+alert(rach);
 
 
 
@@ -15,32 +15,33 @@
     let h = +prompt('Горизонтально чи вертикально (1,2)', '');
     if (h === 1) {
         for (let i = 0; i <= t; i++) {
-            document.write( "*" );
+            document.write("*")
         }
     }
     else if (h === 2) {
         for (let i = 0; i <= t; i++) {
-            document.write( "*" + "<br>" );
+            document.write("*" + "<br>")
         }
     }
 
 
 
-    while (true) {
-        let put = prompt('Вивести зірку?', 'так');
+    while (true){
+        let put = prompt('Вивести зірку?','так');
         if (put.toLowerCase() === "так"){
-            document.write( "*" );
+            document.write("*");
         }
-        else if (put.toLowerCase() === "ні") {
+        else if (put.toLowerCase() === "ні"){
             break;
         }
 
     }
+//
 
     let f;
     let rah = 0;
     while (true) {
-        f = +prompt('Введіть числа', '');
+        f = +prompt('Введіть числа','');
         if (f > 0) {
             rah++;
         }
@@ -48,5 +49,5 @@
             break;
         }
     }
-    console.log( rah );
+    console.log(rah);
 
